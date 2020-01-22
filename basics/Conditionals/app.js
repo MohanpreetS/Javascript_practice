6dff70072ed2a6c59e68a4e2c00338fe0c945040
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-01-22 13:09:04 -0500

    Using conditionals, ticket example

--- basics/Conditionals/app.js
+++ basics/Conditionals/app.js
@@ -27,5 +27,24 @@ if (dayOfWeek === 'monday') {
 
 
 
+// =====================
+// TICKET PRICE EXAMPLE
+// =====================
 
+// 0-5 - FREE
+// 5 - 10 CHILD $10 
+// 10 - 65 ADULT $20 
+// 65+ SENIOR $10
+
+const age = 890;
+
+if (age < 5) {
+    console.log("You are a baby.  You get in for free!")
+} else if (age < 10) {
+    console.log("You are a child.  You pay $10")
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20")
+} else {
+    console.log("You are a senior. You pay $10")
+}
 
