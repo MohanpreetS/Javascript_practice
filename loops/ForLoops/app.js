4c37bddad534ee9eff0b0f98ed48a63a628e0e79
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-01-27 13:09:04 -0500

    For loops intro

---
+++ loops/ForLoops/app.js
@@ -0,0 +1,26 @@
+for (let num = 1; num <= 10; num += 1) {
+    console.log(num);
+}
+
+for (let i = 1; i <= 20; i += 2) {
+    console.log(i)
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
+}
+
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i)
+// }
+
+
+
+
+
