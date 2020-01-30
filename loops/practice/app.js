cedfaf069fcca34c0b3580abdd0342cc50c044b8
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-01-30 13:09:04 -0500

    more while loop practice

--- loops/practice/app.js
+++ loops/practice/app.js
@@ -9,9 +9,35 @@
 //     console.log(arr[i].toUpperCase);
 // }
 
-const SECRET = "password";
+// const SECRET = "password";
 
-let guess = prompt("Enter code");
+// let guess = prompt("Enter code");
+// while (guess !== SECRET) {
+//     guess = prompt("Enter code");
+// }
+
+let count = 0;
+while (count < 10) {
+    count++;
+    console.log(count)
+}
+
+const SECRET = "BabyHippo";
+
+let guess = prompt("enter the secret code...");
 while (guess !== SECRET) {
-    guess = prompt("Enter code");
+    guess = prompt("enter the secret code...");
+}
+console.log("CONGRATS YOU GOT THE SECRET!!!")
+
+let input = prompt("Hey, say something!")
+while (true) {
+    input = prompt(input);
+    if (input.toLowerCase() === "stop copying me") break;
+}
+console.log("OK YOU WIN!")
+
+for (let i = 0; i < 1000; i++) {
+    console.log(i);
+    if (i === 100) break;
 }
