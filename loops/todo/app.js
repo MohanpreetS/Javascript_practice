c1b528d43c988e791cc8672d0308c7f6a5a4b509
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-02-07 13:09:04 -0500

    Todo List p2, delete and view

--- loops/todo/app.js
+++ loops/todo/app.js
@@ -10,6 +10,15 @@ while (true) {
     else if (input.toLowerCase() === "new") {
         let newTodo = prompt("Enter your new todo");
         todoArray.push(newTodo);
-    } 
+    } else if (input.toLowerCase() === "delete") {
+        let todoIndex = prompt("Enter todo index to delete");
+        todoArray.splice(todoIndex, 1);
+    } else if (input.toLowerCase() === "list") {
+        console.log("********");
+        for (let i = 0; i < todoArray.length; i++) {
+            console.log(i, todoArray[i]);
+        }
+        console.log("********");
+    }
     input = prompt("Please type a command");
 }
