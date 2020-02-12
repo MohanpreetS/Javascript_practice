76fdf77e13703d2148f925dff3d409603bc53910
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-02-12 13:09:04 -0500

    Returning functions as outpit

--- functions/ComplexFunctions/app.js
+++ functions/ComplexFunctions/app.js
@@ -17,5 +17,31 @@ function rollDie() {
 callTwice(rollDie)
 
 
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function () {
+            console.log("CONGRATS, I AM A GOOD FUNCTION!")
+            console.log("YOU WIN A MILLION DOLLARS!!")
+        }
+    } else {
+        return function () {
+            alert("YOU HAVE BEEN INFECTED BY A COMPUTER VIRUS!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+        }
+    }
+}
+
+
+function makeBetweenFunc(min, max) {
+    return function (num) {
+        return num >= min && num <= max;
+    }
+}
+
 
 
