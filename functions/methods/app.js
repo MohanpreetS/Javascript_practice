7cc0bf34e333d61cedc453a7170923e36d9a8818
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-02-13 13:09:04 -0500

    Functions as methods in objects, defining and calling

--- functions/methods/app.js
+++ functions/methods/app.js
@@ -8,14 +8,3 @@ const myMath = {
     }
 }
 
-const cat = {
-    name: 'Blue Steele',
-    color: 'grey',
-    breed: 'scottish fold',
-    meow() {
-        console.log("THIS IS:", this)
-        console.log(`${this.name} says MEOWWWW`);
-    }
-}
-
-const meow2 = cat.meow;
