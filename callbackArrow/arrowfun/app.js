48e5ce53861faded9e7acf070f2abf2449561ca1
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-02-19 13:09:04 -0500

    Arrow functions intro

---
+++ callbackArrow/arrowfun/app.js
@@ -0,0 +1,25 @@
+// const add = function(x,y) {
+//     return x + y;
+// }
+
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+const add = (a, b) => a + b;
+
+
+const square = num => {
+    return num * num;
+}
+
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1
+// }
+
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+
+
