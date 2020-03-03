1e11e9115b861321ecd172b45af53cfd5f2e254b
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-03-03 13:09:04 -0500

    Spread op in objects

--- misc/spread/app.js
+++ misc/spread/app.js
@@ -10,3 +10,17 @@ const dogs = ['Rusty', 'Wyatt'];
 
 const allPets = [...cats, ...dogs];
 
+
+// SPREAD IN OBJECTS
+const feline = { legs: 4, family: 'Felidae' };
+const canine = { isFurry: true, family: 'Caninae' };
+
+const catDog = { ...feline, ...canine };
+
+
+const dataFromForm = {
+    email: 'blueman@gmail.com',
+    password: 'tobias123!',
+    username: 'tfunke'
+}
+const newUser = { ...dataFromForm, id: 2345, isAdmin: false }
