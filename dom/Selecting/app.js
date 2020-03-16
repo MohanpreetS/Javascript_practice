e5592a508dd1877f3cf2a508c1c1b6ce5b50e3b7
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-03-16 13:09:04 -0400

    more selecting, removing practice

--- dom/Selecting/app.js
+++ dom/Selecting/app.js
@@ -1,18 +1,25 @@
+const bannerImg = document.getElementById('banner');
+const toc = document.getElementById('toc');
+
 const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
+
+// const allImages = document.getElementsByTagName('img');
+
+// for (let img of allImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+//     console.log(img.src);
+// }
 
 
-const squareImages = document.getElementsByClassName('square');
+// const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-}
+// for (let img of squareImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
 
-const links = document.querySelectorAll('p a');
+// const links = document.querySelectorAll('p a');
 
-for (let link of links) {
-    console.log(link.href)
-}
+// for (let link of links) {
+//     console.log(link.href)
+// }
