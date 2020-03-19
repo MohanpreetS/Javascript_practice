453f457200b4e9b124ac5f73013eeea371af1f2f
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-03-19 13:09:04 -0400

    dom events basics

---
+++ domEvents/EventBasics/app.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('#v2');
+
+btn.onclick = function () {
+    console.log("YOU CLICKED ME!")
+    console.log("I HOPE IT WORKED!!")
+}
+
+function scream() {
+    console.log("AAAAAHHHHH");
+    console.log("STOP TOUCHING ME!")
+}
+
+btn.onmouseenter = scream;
+
+
+document.querySelector('h1').onclick = () => {
+    alert('you clicked the h1!')
+}
+
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', function () {
+    alert("CLICKED!");
+})
+
