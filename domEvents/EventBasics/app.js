136ca2df58e3f335b48422944149ca985e2644a8
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-03-20 13:09:04 -0400

    dom events using functions

--- domEvents/EventBasics/app.js
+++ domEvents/EventBasics/app.js
@@ -23,3 +23,17 @@ btn3.addEventListener('click', function () {
     alert("CLICKED!");
 })
 
+function twist() {
+    console.log("TWIST!")
+}
+function shout() {
+    console.log("SHOUT!")
+}
+
+const tasButton = document.querySelector('#tas');
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist)
+tasButton.addEventListener('click', shout)
