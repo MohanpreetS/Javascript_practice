93ecd77e9ecf4d8abb7a4f2e32d24bc43f7f7e27
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-03-23 13:09:04 -0400

    random colors - solution

--- domEvents/RandomColors/app.js
+++ domEvents/RandomColors/app.js
@@ -1,22 +1,38 @@
-function colorSetter() {
-    let rng1 = Math.floor(Math.random() * 255) + 1;
-    let rng2 = Math.floor(Math.random() * 255) + 1;
-    let rng3 = Math.floor(Math.random() * 255) + 1;
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
 
-    // let clr = rgb(rng1, rng2, rng3);
-    let rclr = `rgb(${rng1}, ${rng2}, ${rng3})`;
-    document.body.style.backgroundColor = rclr;
-    const heading = document.querySelector('#clr');
-    heading.innerText = rclr;
-    if (rng1 <= 30 || rng2 <= 30 || rng3 <= 30) {
-        heading.style.color = 'white';
-    } else {
-        heading.style.color = 'black';
-    }
-    // console.log('printing');
+button.addEventListener('click', function () {
+    const newColor = makeRandColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
-const btn = document.querySelector('#btn');
+// function colorSetter() {
+//     let rng1 = Math.floor(Math.random() * 255) + 1;
+//     let rng2 = Math.floor(Math.random() * 255) + 1;
+//     let rng3 = Math.floor(Math.random() * 255) + 1;
 
-btn.addEventListener('click', colorSetter);
-// btn.addEventListener('click', () => console.log('clicked'));
+//     // let clr = rgb(rng1, rng2, rng3);
+//     let rclr = `rgb(${rng1}, ${rng2}, ${rng3})`;
+//     document.body.style.backgroundColor = rclr;
+//     const heading = document.querySelector('#clr');
+//     heading.innerText = rclr;
+//     if (rng1 <= 30 || rng2 <= 30 || rng3 <= 30) {
+//         heading.style.color = 'white';
+//     } else {
+//         heading.style.color = 'black';
+//     }
+//     // console.log('printing');
+// }
+
+// const btn = document.querySelector('#btn');
+
+// btn.addEventListener('click', colorSetter);
+// // btn.addEventListener('click', () => console.log('clicked'));
