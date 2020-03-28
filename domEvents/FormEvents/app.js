f4e372b9870c401d44dc9620989425855437a34e
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-03-28 13:09:04 -0400

    adding tweet

--- domEvents/FormEvents/app.js
+++ domEvents/FormEvents/app.js
@@ -5,8 +5,12 @@ const tweetsContainer = document.querySelector('#tweets');
 tweetForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const usernameInput = document.querySelectorAll('input')[0];
-    const tweetInput = document.querySelectorAll('input')[1];
-
+    // const usernameInput = document.querySelectorAll('input')[0];
+    // const tweetInput = document.querySelectorAll('input')[1];
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+    addTweet(usernameInput.value, tweetInput.value)
+    usernameInput.value = '';
+    tweetInput.value = '';
 });
 
