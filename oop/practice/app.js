f077d881215a86ec6d0b349025aa507344216bcc
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-05-15 13:09:04 -0400

    practicing classes, adding methods

---
+++ oop/practice/app.js
@@ -0,0 +1,35 @@
+class Color {
+	constructor(r, g, b, name) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.name = name;
+		this.calcHSL();
+	}
+	innerRGB() {
+		const { r, g, b } = this;
+		return `${r}, ${g}, ${b}`;
+	}
+	rgb() {
+		return `rgb(${this.innerRGB()})`;
+	}
+	rgba(a = 1.0) {
+		return `rgba(${this.innerRGB()}, ${a})`;
+	}
+	hex() {
+		const { r, g, b } = this;
+		return (
+			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+		);
+	}
+	hsl() {
+		const { h, s, l } = this;
+		return `hsl(${h},${s}%, ${l}%)`;
+	}
+	fulllySaturated() {
+		const { h, l } = this;
+		return `hsl(${h},100%, ${l}%)`;
+	}
+	
+}
+
